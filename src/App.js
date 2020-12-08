7e471a0e2e84211b15300447c21588040efd5824
Mahesh Vagicherla <59088937+b4s36t4@users.noreply.github.com>
2020-12-08 14:39:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import NavBar from  './header';
+import './styles/tailwind.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
